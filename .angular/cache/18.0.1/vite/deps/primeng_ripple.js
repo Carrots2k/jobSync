a21fd72cc482b99a25f999258b16830d21817791
Aubz <aubreytmb@gmail.com>
2024-05-31 16:28:25 +0200

    added the sidebar

---
+++ .angular/cache/18.0.1/vite/deps/primeng_ripple.js
@@ -0,0 +1,13 @@
+import {
+  Ripple,
+  RippleModule
+} from "./chunk-C46NZSSV.js";
+import "./chunk-TKJMWDM5.js";
+import "./chunk-DBLSY2FX.js";
+import "./chunk-CMHO36X6.js";
+import "./chunk-UW4MIM2Y.js";
+export {
+  Ripple,
+  RippleModule
+};
+//# sourceMappingURL=primeng_ripple.js.map

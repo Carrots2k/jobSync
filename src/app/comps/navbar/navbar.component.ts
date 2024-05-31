aa30bbd89e37a3c38036ddd0df243e31c15a3722
Aubz <aubreytmb@gmail.com>
2024-05-31 23:38:08 +0200

    Refactored the navbar amd the sidebar

--- src/app/comps/navbar/navbar.component.ts
+++ src/app/comps/navbar/navbar.component.ts
@@ -5,11 +5,12 @@ import { AvatarModule } from 'primeng/avatar';
 import { InputTextModule } from 'primeng/inputtext';
 import { CommonModule } from '@angular/common';
 import { RippleModule } from 'primeng/ripple';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MenubarModule, BadgeModule, AvatarModule, InputTextModule, RippleModule, CommonModule],
+  imports: [MenubarModule, BadgeModule, AvatarModule, InputTextModule, RippleModule, CommonModule, ButtonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })

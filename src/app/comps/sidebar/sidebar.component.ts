60585481fa59c3cfb2cbb248b7ce2db321c0668c
Aubz <aubreytmb@gmail.com>
2024-06-24 12:20:09 +0200

    refactored code

--- src/app/comps/sidebar/sidebar.component.ts
+++ src/app/comps/sidebar/sidebar.component.ts
@@ -2,17 +2,16 @@ import { Component, ViewChild } from '@angular/core';
 import { SidebarModule } from 'primeng/sidebar';
 import { ButtonModule } from 'primeng/button';
 import { RippleModule } from 'primeng/ripple';
-import { AvatarModule } from 'primeng/avatar';
 import { StyleClassModule } from 'primeng/styleclass';
 import { Sidebar } from 'primeng/sidebar';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { SidebarService } from '../../services/sidebar.service';
-import { Router } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [SidebarModule, ButtonModule, RippleModule, AvatarModule, StyleClassModule, NavbarComponent],
+  imports: [SidebarModule, ButtonModule, RippleModule, StyleClassModule, NavbarComponent, RouterLink],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
@@ -20,22 +19,13 @@ export class SidebarComponent {
   @ViewChild('sidebarRef') sidebarRef!: Sidebar;
 
   sidebarVisible: boolean = true;
-  routerLink : string  = '/home'
   
-  constructor(private sidebarService: SidebarService,private router: Router) {
+  constructor(private sidebarService: SidebarService) {
     this.sidebarService.sidebarToggle$.subscribe(() => {
       this.sidebarVisible = !this.sidebarVisible;
     });
   }
 
   goHome(){
-    console.log('going to home')
-    this.router.navigate(['/home'])
-  }
-
-  goJobs(){
-    console.log('going to jobs')
-    this.router.navigate(['/jobs'])
-  }
 
 }

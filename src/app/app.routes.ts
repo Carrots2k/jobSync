5deb95f1af5c3623ee3f3daa093a4c1e4b621a1e
Aubz <aubreytmb@gmail.com>
2024-06-26 17:16:25 +0200

    Created jobcard and job mananebt comps,
    updated the side and router links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,17 @@ import { Routes } from '@angular/router';
 import { MainlayoutComponent } from './layout/mainlayout/mainlayout.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { JobsComponent } from './pages/jobs/jobs.component';
+import { JobManagementComponent } from './comps/job-management/job-management.component';
+import { JobcardComponent } from './comps/jobcard/jobcard.component';
 
 export const routes: Routes = [
     {path: '', component: MainlayoutComponent,
         children: [
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             {path: 'home', component: DashboardComponent},
-            {path: 'jobs', component: JobsComponent}
+            {path: 'jobs', component: JobsComponent},
+            {path: 'jobcard', component: JobcardComponent},
+            {path: 'jobs-management', component: JobManagementComponent}
         ]
     }
 ];
